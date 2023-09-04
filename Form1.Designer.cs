ae715536fb1f199aad5f38c393f0b07a8388d923
Rian <riansilvasantos04@gmail.com>
2023-09-04 20:20:11 -0300

    um programa para calcular o imc

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ex4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.altura = new System.Windows.Forms.TextBox();
+            this.peso = new System.Windows.Forms.TextBox();
+            this.botao = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "altura";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "peso";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // altura
+            // 
+            this.altura.Location = new System.Drawing.Point(306, 80);
+            this.altura.Name = "altura";
+            this.altura.Size = new System.Drawing.Size(100, 22);
+            this.altura.TabIndex = 2;
+            this.altura.TextChanged += new System.EventHandler(this.altura_TextChanged);
+            // 
+            // peso
+            // 
+            this.peso.Location = new System.Drawing.Point(306, 178);
+            this.peso.Name = "peso";
+            this.peso.Size = new System.Drawing.Size(100, 22);
+            this.peso.TabIndex = 3;
+            this.peso.TextChanged += new System.EventHandler(this.peso_TextChanged);
+            // 
+            // botao
+            // 
+            this.botao.Location = new System.Drawing.Point(316, 265);
+            this.botao.Name = "botao";
+            this.botao.Size = new System.Drawing.Size(75, 23);
+            this.botao.TabIndex = 4;
+            this.botao.Text = "calcular";
+            this.botao.UseVisualStyleBackColor = true;
+            this.botao.Click += new System.EventHandler(this.botao_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(535, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.botao);
+            this.Controls.Add(this.peso);
+            this.Controls.Add(this.altura);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox altura;
+        private System.Windows.Forms.TextBox peso;
+        private System.Windows.Forms.Button botao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botao_Click(object sender, EventArgs e)
+        {
+            double altura1 = double.Parse(altura.Text);
+            double peso1 = double.Parse(peso.Text);
+            double r = Math.Pow(altura1, 2);
+            double imc = peso1/r;
+            string result = "";
+            
+            label3.Text = imc.ToString("F");
+            if(imc <= 18.4) { 
+                result = "voce esta abaixo do peso ideal";
+
+
+
+            }else if(imc >= 18.5 && imc <= 24.9) {
+                result = "voce esta com peso normal";
+            }else
+            {
+                result = "voce esta acima do peso ideal";
+            }
+            label4.Text= result;
+
+        }
+
+        private void peso_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void altura_TextChanged(object sender,EventArgs e)
+        {
+
+            
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
